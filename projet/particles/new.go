64d2e8d76da030591c10c17456204159da8ddfae
E224508F <bryan.levy@etu.univ-nantes.fr>
2022-12-14 14:52:07 +0100

    ajout commentaire

--- projet/particles/new.go
+++ projet/particles/new.go
@@ -12,12 +12,14 @@ import (
 // centre de l'écran.
 func NewSystem() System {
 	l := list.New()
+	//
 	l.PushFront(&Particle{
 		PositionX: float64(config.General.WindowSizeX) / 2,
 		PositionY: float64(config.General.WindowSizeY) / 2,
-		ScaleX:    1, ScaleY: 1,
+		ScaleX:    1, ScaleY: 1,										//Partie à remplacer
 		ColorRed: 1, ColorGreen: 1, ColorBlue: 1,
 		Opacity: 1,
 	})
+	//
 	return System{Content: l}
 }
