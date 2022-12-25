17f1832b20ea21da4eb1989506c78ef2907f2c0f
Bini <bryan.levy@etu.univ-nantes.fr>
2022-12-25 18:20:03 +0100

    Gestion de l'option SpawnRate

--- projet/particles/update.go
+++ projet/particles/update.go
@@ -3,6 +3,8 @@ package particles
 import (
 	"container/list"
 	"fmt"
+	"math/rand"
+	"project-particles/config"
 )
 
 // Update mets à jour l'état du système de particules (c'est-à-dire l'état de
@@ -10,6 +12,8 @@ import (
 // 60 fois par seconde (de manière régulière) par la fonction principale du
 // projet.
 // C'est à vous de développer cette fonction.
+var nb float64
+
 func (s *System) Update() {
 	var ele *Particle
 	var myList *list.List = s.Content
@@ -19,5 +23,40 @@ func (s *System) Update() {
 		ele.PositionX = ele.PositionX + ele.SpeedX
 		ele.PositionY = ele.PositionY + ele.SpeedY
 	}
+	var a float64 = config.General.SpawnRate
+	nb = nb + a - float64(int(a))
+	if nb > 1 {
+		nb = nb - 1
+		a = a + 1
+	}
+	createNParticles(int(a), myList)
 
+}
+
+func createNParticles(nb int, l *list.List) *list.List {
+	for i := 0; i < nb; i++ {
+		var posX float64 = float64(config.General.SpawnX)
+		var posY float64 = float64(config.General.SpawnY)
+		var speedX float64 = rand.Float64() * config.General.Velocity
+		var speedY float64 = rand.Float64() * config.General.Velocity
+		var signe []int = []int{-1, 1}
+		speedX = speedX * float64(signe[rand.Intn(2)])
+		speedY = speedY * float64(signe[rand.Intn(2)])
+
+		if config.General.RandomSpawn {
+			posX = float64(rand.Intn(config.General.WindowSizeX))
+			posY = float64(rand.Intn(config.General.WindowSizeY))
+
+		}
+		l.PushFront(&Particle{
+			PositionX: float64(posX),
+			PositionY: float64(posY),
+			ScaleX:    config.General.ScaleX, ScaleY: config.General.ScaleY, //Partie à remplacer
+			ColorRed: config.General.ColorRed / 255, ColorGreen: config.General.ColorGreen / 255, ColorBlue: config.General.ColorBlue / 255,
+			Opacity: config.General.Opacity,
+			SpeedX:  speedX,
+			SpeedY:  speedY,
+		})
+	}
+	return l
 }
